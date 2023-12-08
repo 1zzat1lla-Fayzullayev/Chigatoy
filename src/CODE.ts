023d412b35d6f416acd91da471d835e067b806a2
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2023-12-08 09:49:12 +0500

    card

--- src/CODE.ts
+++ src/CODE.ts
@@ -1,12 +1,39 @@
-import image from './assets/chigatoy.jpg'
-export default  {
+import salat1 from "./assets/salat1.jpg";
+import salat2 from "./assets/salat2.jpg";
+import salat3 from "./assets/salat3.jpg";
+import salat4 from "./assets/salat4.jpg";
+import salat5 from "./assets/salat5.jpg";
+import salat6 from "./assets/salat6.jpg";
+import salat7 from "./assets/salat7.jpg";
+import salat8 from "./assets/salat8.jpg";
+import salat9 from "./assets/salat9.jpg";
+import shashlik1 from "./assets/shashlik1.jpg";
+import shashlik2 from "./assets/shashlik2.jpg";
+import shashlik3 from "./assets/shashlik3.jpg";
+import shashlik4 from "./assets/shashlik4.jpg";
+import shashlik5 from "./assets/shashlik5.jpg";
+import shashlik6 from "./assets/shashlik6.jpg";
+import shashlik7 from "./assets/shashlik7.jpg";
+import shashlik8 from "./assets/shashlik8.jpg";
+import shashlik9 from "./assets/shashlik9.jpg";
+import shorva1 from "./assets/shorva1.jpg";
+import shorva2 from "./assets/shorva2.jpg";
+import shorva3 from "./assets/shorva3.jpg";
+import shorva4 from "./assets/shorva4.jpg";
+import shorva5 from "./assets/shorva5.jpg";
+import shorva6 from "./assets/shorva6.jpg";
+import shorva7 from "./assets/shorva7.jpg";
+import shorva8 from "./assets/shorva8.jpg";
+import shorva9 from "./assets/shorva9.jpg";
+
+export default {
   menu: [
     {
       name: "Vegie Muffen",
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat1,
       rating: 5,
     },
     {
@@ -14,7 +41,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat2,
       rating: 5,
     },
     {
@@ -22,7 +49,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat3,
       rating: 5,
     },
     {
@@ -30,7 +57,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat4,
       rating: 5,
     },
     {
@@ -38,7 +65,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat5,
       rating: 5,
     },
     {
@@ -46,7 +73,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat6,
       rating: 5,
     },
     {
@@ -54,7 +81,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat7,
       rating: 5,
     },
     {
@@ -62,7 +89,7 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat8,
       rating: 5,
     },
     {
@@ -70,7 +97,151 @@ export default  {
       description:
         "There are many things are needed to start the Fast Food Business.",
       price: 10,
-      image: image,
+      image: salat9,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik1,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik2,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik3,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik4,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik5,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik6,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik7,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik8,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shashlik9,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva1,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva2,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva3,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva4,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva5,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva6,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva7,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva8,
+      rating: 5,
+    },
+    {
+      name: "Vegie Muffen",
+      description:
+        "There are many things are needed to start the Fast Food Business.",
+      price: 10,
+      image: shorva9,
       rating: 5,
     },
   ],
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ import CODE from "../CODE";
 function Menu(): JSX.Element {
   return (
     <>
-      <div className="container menu">
+      <div className="container menu_ota">
         <p className="menu_p">MENU</p>
         <h3>Food Full of treaty Love</h3>
         <p>
@@ -11,20 +11,35 @@ function Menu(): JSX.Element {
           need not only Just Food Stalls with Persons but also specialized
           equipment, Skills to manage Customers
         </p>
-        <div className="row">
-          {
-            CODE.menu.map((menu, i) => {
-              return (
-                <div className="col-12 col-md-4" key={i}>
-                  <div className="card food_card">
-                    <div className="card-img">
-                      <img src={menu.image} alt="404" />
+        <div className="row menu">
+          {CODE.menu.map((menu, i) => {
+            return (
+              <div className="col-12 col-md-4 menu_col" key={i}>
+                <div className="menu-box">
+                  <div className="menu_box">
+                    <div className="menu_card">
+                      <div className="menu_image">
+                        <img src={menu.image} />
+                      </div>
+
+                      <div className="small_card">
+                        <i className="fa-solid fa-heart"></i>
+                      </div>
+
+                      <div className="menu_info">
+                        <h2>{menu.name}</h2>
+                        <p>{menu.description}</p>
+                        <div className="priceandbtn">
+                          <h3>${menu.price}</h3>
+                          <i className="bx bxs-plus-circle"></i>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              )
-            })
-          }
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
