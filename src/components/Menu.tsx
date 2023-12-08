5c177ea10a4bdef94b93ea76d956b199659eed7e
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2023-12-08 08:13:07 +0500

    update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,20 +2,18 @@ import "../sass/_menu.scss";
 function Menu(): JSX.Element {
   return (
     <>
-      <div className="container">
-        <p>MENU</p>
+      <div className="container menu">
+        <p className="menu_p">MENU</p>
         <h3>Food Full of treaty Love</h3>
         <p>
           There are many things are needed to start the Fast Food Business. You
           need not only Just Food Stalls with Persons but also specialized
-          equipment, Skills to manage Customers,{" "}
+          equipment, Skills to manage Customers
         </p>
         <div className="row">
           <div className="col">
             <div className="food_card">
-                <div className="card_image">
-                    
-                </div>
+              <div className="card_image"></div>
             </div>
           </div>
         </div>
