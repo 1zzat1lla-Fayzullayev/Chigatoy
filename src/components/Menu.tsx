1f43a17f64297279f40f07f00cef21095af73d5f
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2023-12-12 16:28:25 +0500

    add

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,7 @@
 import "../sass/_menu.scss";
-import CODE from "../CODE";
+import SALAT from "../ts/SALAT";
+import SHORVA from "../ts/SHORVA";
+import SHASHLIK from "../ts/SHASHLIK";
 function Menu(): JSX.Element {
   return (
     <>
@@ -12,7 +14,66 @@ function Menu(): JSX.Element {
           equipment, Skills to manage Customers
         </p>
         <div className="row menu">
-          {CODE.menu.map((menu, i) => {
+          <h1>Salat</h1>
+          {SALAT.menu.map((menu, i) => {
+            return (
+              <div className="col-lg-3 col-md-5 col-sm-12 menu_col" key={i}>
+                <div className="menu-box">
+                  <div className="menu_box">
+                    <div className="menu_card">
+                      <div className="menu_image">
+                        <img src={menu.image} />
+                      </div>
+
+                      <div className="small_card">
+                        <i className="fa-solid fa-heart"></i>
+                      </div>
+
+                      <div className="menu_info">
+                        <h4>{menu.name}</h4>
+                        {/* <p>{menu.description}</p> */}
+                        <div className="priceandbtn">
+                          <button>{menu.price},000 сум</button>
+                          <i className="bx bxs-plus-circle"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <h1>Shorva</h1>
+          {SHORVA.shorva.map((menu, i) => {
+            return (
+              <div className="col-lg-3 col-md-5 col-sm-12 menu_col" key={i}>
+                <div className="menu-box">
+                  <div className="menu_box">
+                    <div className="menu_card">
+                      <div className="menu_image">
+                        <img src={menu.image} />
+                      </div>
+
+                      <div className="small_card">
+                        <i className="fa-solid fa-heart"></i>
+                      </div>
+
+                      <div className="menu_info">
+                        <h4>{menu.name}</h4>
+                        {/* <p>{menu.description}</p> */}
+                        <div className="priceandbtn">
+                          <button>{menu.price},000 сум</button>
+                          <i className="bx bxs-plus-circle"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <h1>Shashlik</h1>
+          {SHASHLIK.shashlik.map((menu, i) => {
             return (
               <div className="col-lg-3 col-md-5 col-sm-12 menu_col" key={i}>
                 <div className="menu-box">
---
+++ src/ts/SALAT.ts
@@ -0,0 +1,85 @@
+import salat1 from "../assets/salat1.jpg";
+import salat2 from "../assets/salat2.jpg";
+import salat3 from "../assets/salat3.jpg";
+import salat4 from "../assets/salat4.jpg";
+import salat5 from "../assets/salat5.jpg";
+import salat6 from "../assets/salat6.jpg";
+import salat7 from "../assets/salat7.jpg";
+import salat8 from "../assets/salat8.jpg";
+import salat9 from "../assets/salat9.jpg";
+
+
+export default {
+  menu: [
+    {
+      name: "Салат Японский",
+      description:
+        "МЯСО,ЖАРЕНЫЙ ЛУК,ЧЕСНОК, ОГУРЕЦ, ПОМИДОР,БОЛГАРСКИЙ ПЕРЕЦ ,КРАСНЫЙ ПЕРЕЦ,ЧЕРНЫЙ ПЕРЕЦ,COLЛЬGO'SHT, QOVURILGAN PIYOZ, SARIMSOQ, BODRING, POMIDOR, BOLGAR QALAMPIRI, QIZIL QALAMPIR, QORA MURCH, TUZ",
+      price: 35,
+      image: salat1,
+      rating: 5,
+    },
+    {
+      name: "Селёдка",
+      description:
+        "СЕЛЁДКА, ОТВАРНОЙ КАРТОФЕЛЬ, МОРКОВЬ ОТВАРНОЙ, ЛУК ,ПЕРЕЦ И СОЛЬSELD, QAYNATILGAN KARTOSHKA, QAYNATILGAN SABZI, PIYOZ, QALAMPIR VA TUZ",
+      price: 35,
+      image: salat2,
+      rating: 5,
+    },
+    {
+      name: "Cмак",
+      description:
+        "ПОМИДОР, СЫР, БЕЛЫЙ ХЛЕБ, ЧЕСНОК, МАЙОНЕЗ, УКРОП, ПЕРЕЦ, СОЛЬ, POMIDOR, PISHLOQ, OQ NON, SARIMSOQ, MAYONEZ, ARPABODIYON, QALAMPIR, TUZ",
+      price: 35,
+      image: salat3,
+      rating: 5,
+    },
+    {
+      name: "Сузьма",
+      description: "Сузьма, Suzma",
+      price: 10,
+      image: salat4,
+      rating: 5,
+    },
+    {
+      name: "Аччик чучук",
+      description:
+        "Помидор зелень лук специи Pomidor ko'katlar piyoz ziravorlar",
+      price: 15,
+      image: salat5,
+      rating: 5,
+    },
+    {
+      name: "Соленья",
+      description: "Соленья",
+      price: 10,
+      image: salat6,
+      rating: 5,
+    },
+    {
+      name: "Свежее ассорти",
+      description: "Помидоры и огурцы Pomidor va bodring",
+      price: 15,
+      image: salat7,
+      rating: 5,
+    },
+    {
+      name: "Салат Мужской капри",
+      description: "Салат Мужской каприз",
+      price: 35,
+      image: salat8,
+      rating: 5,
+    },
+    {
+      name: "Оливье",
+      description:
+        "ОТВАРНОЕ МЯСО,ОТВАРНОЙ КАРТОФЕЛЬ,МОРКОВЬ, ОГУРЦЫ,ЗЕЛЕНЫЙ ГОРОШЕК, МАЙОНЕЗ QAYNATILGAN GO'SHT, QAYNATILGAN KARTOSHKA, SABZI, BODRING, YASHIL NO'XAT, MAYONEZ",
+      price: 35,
+      image: salat9,
+      rating: 5,
+    },
+
+
+  ],
+};
