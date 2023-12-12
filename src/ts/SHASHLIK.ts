cbd7933bb541b2de99ee02167c6d35b9b94692f7
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2023-12-12 16:28:25 +0500

    add

---
+++ src/ts/SHASHLIK.ts
@@ -0,0 +1,85 @@
+import shashlik1 from "../assets/shashlik1.jpg";
+import shashlik2 from "../assets/shashlik2.jpg";
+import shashlik3 from "../assets/shashlik3.jpg";
+import shashlik4 from "../assets/shashlik4.jpg";
+import shashlik5 from "../assets/shashlik5.jpg";
+import shashlik6 from "../assets/shashlik6.jpg";
+import shashlik7 from "../assets/shashlik7.jpg";
+import shashlik8 from "../assets/shashlik8.jpg";
+import shashlik9 from "../assets/shashlik9.jpg";
+export default {
+    shashlik: [
+        {
+            name: "Шашлык из конины",
+            description:
+                "Шашлык из конины Qazi kabob",
+            price: 18,
+            image: shashlik1,
+            rating: 5,
+        },
+        {
+            name: "Шашлык говяжий кусковой",
+            description:
+                "Шашлык говяжий кусковой Jaz kabob mol gosht 1 dona",
+            price: 18,
+            image: shashlik2,
+            rating: 5,
+        },
+        {
+            name: "Шашлык из баранины кусковой",
+            description:
+                "Шашлык из баранины кусковой 1 шт. Jaz qoy goshtli kabob 1 dona",
+            price: 18,
+            image: shashlik3,
+            rating: 5,
+        },
+        {
+            name: "Рулет кусковой",
+            description:
+                "Рулет кусковой  1 шт. Jiz kabob 1 dona",
+            price: 30,
+            image: shashlik4,
+            rating: 5,
+        },
+        {
+            name: "Шашлык молотый",
+            description:
+                "Шашлык молотый 1 шт. Qiyma kabob 1 dona",
+            price: 18,
+            image: shashlik5,
+            rating: 5,
+        },
+        {
+            name: "Шашлык три панжа",
+            description:
+                "Шашлык три панжа Uch panja kabob",
+            price: 65,
+            image: shashlik6,
+            rating: 5,
+        },
+        {
+            name: "Шашлык из курицы",
+            description:
+                "Шашлык из курицы 1 шт. Tovuq kaboblari 1 dona.",
+            price: 18,
+            image: shashlik7,
+            rating: 5,
+        },
+        {
+            name: "Шашлык из сома",
+            description:
+                "Шашлык из сома 1 шт. Baliq kaboblari 1 dona.",
+            price: 18,
+            image: shashlik8,
+            rating: 5,
+        },
+        {
+            name: "Шашлык из печени",
+            description:
+                "Шашлык из печени 1 шт Jigar kabob 1 dona ",
+            price: 18,
+            image: shashlik9,
+            rating: 5,
+        },
+    ]
+}
